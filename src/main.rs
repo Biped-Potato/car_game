0e16ca79b0017eb077f424d3fd755dd9e4ebfd75
Platypus88 <79812179+Platypus88@users.noreply.github.com>
2023-07-13 11:19:16 -0500

    camera follow and impulse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy_rapier3d::prelude::*;
+use rand::Rng;
+use rand::rngs::ThreadRng;
 
 fn main() {
     App::new()
@@ -13,7 +15,7 @@ fn main() {
             DefaultPlugins
         )
         .add_plugin(
-            RapierPhysicsPlugin::<NoUserData>::default()
+            RapierPhysicsPlugin::<NoUserData>::default().with_physics_scale(10.)
         )
         .add_plugin(
             RapierDebugRenderPlugin::default()
@@ -22,6 +24,7 @@ fn main() {
         .add_startup_system(setup_physics)
         .add_system(update_car_suspension)
         .add_system(camera_follow)
+        .add_system(car_controls)
         .run();
 }
 #[derive(Component)]
@@ -43,9 +46,7 @@ fn setup_graphics(mut commands: Commands) {
 }
 
 pub fn setup_physics(mut commands: Commands) {
-    /*
-     * Ground
-     */
+
     let ground_size = 200.1;
     let ground_height = 0.1;
 
@@ -54,16 +55,19 @@ pub fn setup_physics(mut commands: Commands) {
         Collider::cuboid(ground_size, ground_height, ground_size),
     ));
 
-    /*
-     * Create the cubes
-     */
     let car_size = Vec3::new(5.,3.,7.);
 
     commands.spawn((
         TransformBundle::from(Transform::from_xyz(0., 5., 0.)),
         RigidBody::Dynamic,
         Collider::cuboid(car_size.x, car_size.y, car_size.z),
-    )).insert(CarPhysics{});
+    ))
+    .insert(CarPhysics{})
+    .insert(ExternalImpulse {
+        impulse: Vec3::new(0., 0., 0.),
+        torque_impulse: Vec3::new(0., 0., 0.),
+    })
+    .insert(GravityScale(10.));
 }
 pub fn camera_follow(mut car_query: Query<(&mut CarPhysics,&mut Transform),Without<CameraFollow>>,mut camera_query:Query<(&CameraFollow,&mut Transform),Without<CarPhysics>>)
 {   
@@ -72,42 +76,71 @@ pub fn camera_follow(mut car_query: Query<(&mut CarPhysics,&mut Transform),Witho
         if let Ok((car_physics,mut car_transform)) = car_query.get_single_mut()
         {
             camera_transform.translation = car_transform.translation +(car_transform.back()+Vec3::new(0.,0.3,0.)).normalize()*camera_follow.distance_behind;
-            camera_transform.rotation = car_transform.rotation;
-            camera_transform.rotate_x(-10.*0.0174533);
+            camera_transform.look_at(car_transform.translation,Vec3::Y);
+            camera_transform.translation.y +=13.;
+            //camera_transform.rotation = car_transform.rotation;
+            //camera_transform.rotate_x(-10.*0.0174533);
         }
     }
     
+}
+pub fn neg_or_pos(rng : &mut ThreadRng) -> i32
+{
+    if rng.gen_range(0..2) == 1
+    {
+        return 1;
+    }
+    return -1;
+}
+pub fn car_controls(keys: Res<Input<KeyCode>>,mut car_query: Query<(&mut CarPhysics,&mut ExternalImpulse,&mut Transform),Without<CameraFollow>>)
+{
+    if let Ok((car_physics,mut impulse,mut car_transform)) = car_query.get_single_mut()
+    {
+        if keys.just_pressed(KeyCode::Space)
+        {
+            let mut rng = rand::thread_rng();
+            impulse.impulse = ExternalImpulse::at_point(Vec3::new(0.,0.01,0.),car_transform.translation,car_transform.translation).impulse;
+            //impulse.impulse = Vec3::new(0.,0.01,0.);
+            impulse.torque_impulse = Vec3::new(
+                rng.gen_range(1.0..3.0)*neg_or_pos(&mut rng) as f32,
+                rng.gen_range(1.0..3.0)*neg_or_pos(&mut rng) as f32,
+                rng.gen_range(1.0..3.0)*neg_or_pos(&mut rng) as f32
+            );
+        }
+        
+    }
 }
 fn update_car_suspension(
     mut commands: Commands,
     windows: Query<&Window, With<PrimaryWindow>>,
     rapier_context: Res<RapierContext>,
-    cameras: Query<(&Camera, &GlobalTransform)>,
+    mut car_query: Query<(&mut CarPhysics,&mut ExternalImpulse,&mut Transform),Without<CameraFollow>>
 ) {
-    let window = windows.single();
+    if let Ok((car_physics,mut impulse,mut car_transform)) = car_query.get_single_mut()
+    {
+        let window = windows.single();
 
-    let Some(cursor_position) = window.cursor_position() else { return; };
+        let Some(cursor_position) = window.cursor_position() else { return; };
 
-    // We will color in read the colliders hovered by the mouse.
-    for (camera, camera_transform) in &cameras {
-        // First, compute a ray from the mouse position.
-        let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else { return; };
+        // We will color in read the colliders hovered by the mouse.
+        for (camera, camera_transform) in &cameras {
 
-        // Then cast the ray.
-        let hit = rapier_context.cast_ray(
-            ray.origin,
-            ray.direction,
-            f32::MAX,
-            true,
-            QueryFilter::only_dynamic(),
-        );
+            // Then cast the ray.
+            let hit = rapier_context.cast_ray(
+                ray.origin,
+                ray.direction,
+                f32::MAX,
+                true,
+                QueryFilter::only_dynamic(),
+            );
 
-        if let Some((entity, _toi)) = hit {
-            // Color in blue the entity we just hit.
-            // Because of the query filter, only colliders attached to a dynamic body
-            // will get an event.
-            let color = Color::BLUE;
-            commands.entity(entity).insert(ColliderDebugColor(color));
+            if let Some((entity, _toi)) = hit {
+                // Color in blue the entity we just hit.
+                // Because of the query filter, only colliders attached to a dynamic body
+                // will get an event.
+                let color = Color::BLUE;
+                commands.entity(entity).insert(ColliderDebugColor(color));
+            }
         }
     }
 }
